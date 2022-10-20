c6df8d0b196c7ea30b3c3a8f5973dc504c11af2e
Alexandre Pedro <alexandrecpedro@gmail.com>
2022-10-19 22:35:58 -0300

    Class 01 - From JavaScript Vanilla to React

---
+++ digital-house/src/App.jsx
@@ -0,0 +1,13 @@
+import { SecondClass } from "./classes/SecondClass";
+import { Login } from "./pages/Login";
+
+function App() {
+  return (
+    <>
+      {/* <Login /> */}
+      <SecondClass />
+    </>
+  );
+}
+
+export default App;
---
+++ digital-house/src/classes/SecondClass/index.jsx
@@ -0,0 +1,16 @@
+import './style.scss'
+
+export function SecondClass() {
+    return (
+        <main>
+            <h1>Second Class</h1>
+            <h2>Chosen project: Instagram</h2>
+            <ul>
+                <li>
+                    <h2>First component</h2>
+                    <p></p>
+                </li>
+            </ul>
+        </main>
+    )
+}
