a7bdb5e1a0e4ff87f3520137b285d6d0d75e64da
Alexandre Pedro <alexandrecpedro@gmail.com>
2022-10-24 22:47:01 -0300

    Class 04 - Thinking about React

---
+++ digital-house/src/lessons/FourthClass/index.jsx
@@ -0,0 +1,20 @@
+import './style.scss'
+import { componentsFinded } from './database'
+
+export function FourthClass(props) {
+    return (
+		<div className='fourth-class-component'>
+        	<h1 className='main-title'>Identified components</h1>
+
+			<ul className='components-finded'>
+				{componentsFinded.map((component) => (
+					<li key={component.id}>
+						<img src={component.image} alt={component.imageAlt} />
+						<h1>{component.title}</h1>
+						<p>{component.description}</p>
+					</li>
+				))}
+			</ul>
+		</div>
+    )
+}
