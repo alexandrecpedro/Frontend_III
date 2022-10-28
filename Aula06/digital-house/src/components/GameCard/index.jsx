f4b4724bd78199e05faf9e9a86c3448f38e995ec
Alexandre Pedro <alexandrecpedro@gmail.com>
2022-10-28 12:36:38 -0300

    Class 06 - Working with components [review]

--- Aula06/digital-house/src/components/GameCard/index.jsx
+++ Aula06/digital-house/src/components/GameCard/index.jsx
@@ -2,16 +2,16 @@ import "./style.scss";
 
 export function GameCard({ name, platforms, categories, picture, price }) {
   return (
-    <div className="game-card">
+    <li className="game-card">
       <img className="game-card-image" src={picture} />
       <section className="game-card-text">
-        <article className="description">
+        <article className="game-card-lines">
             <h3 className="game-card-title">{name}</h3>
             <div className="game-card-platforms">{platforms}</div>
             <h4 className="game-card-categories">{categories}</h4>
         </article>
         <article className="game-card-price">{price}</article>
       </section>
-    </div>
+    </li>
   );
 }
--- digital-house/src/components/OfferCard/index.jsx
+++ digital-house/src/components/OfferCard/index.jsx
@@ -2,18 +2,18 @@ import "./style.scss";
 
 export function OfferCard({ name, categories, oldPrice, newPrice, picture }) {
   return (
-    <div className="offer-card">
+    <li className="offer-card">
       <img className="offer-offer-image" src={picture} />
       <section className="offer-card-text">
-        <article className="description">
-            <h3 className="offer-card-title">{name}</h3>
-            <h4 className="offer-card-categories">{categories}</h4>
+        <article className="offer-card-lines">
+          <h3 className="offer-card-title">{name}</h3>
+          <p className="old-price">{oldPrice}</p>
         </article>
         <article className="offer-card-prices">
-          <p className="old-price">{oldPrice}</p>
+          <h4 className="offer-card-categories">{categories}</h4>
           <p className="new-price">{newPrice}</p>
         </article>
       </section>
-    </div>
+    </li>
   );
 }
