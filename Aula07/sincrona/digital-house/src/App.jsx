3bd632149228f02c3119d840ed6c8d06308c7773
Alexandre Pedro <alexandrecpedro@gmail.com>
2022-10-31 23:04:01 -0300

    Class 07 - React Hooks

---
+++ Aula07/sincrona/digital-house/src/App.jsx
@@ -0,0 +1,21 @@
+import { FifthClass } from "./lessons/FifthClass";
+import { FourthClass } from "./lessons/FourthClass";
+import { SecondClass } from "./lessons/SecondClass";
+import { ThirdClass } from "./lessons/ThirdClass";
+import { DHGames } from "./pages/DHGames";
+import { Login } from "./pages/Login";
+
+function App() {
+  return (
+    <>
+      {/* <Login /> */}
+      {/* <SecondClass /> */}
+      {/* <ThirdClass /> */}
+      {/* <FourthClass /> */}
+      {/* <FifthClass /> */}
+      <DHGames />
+    </>
+  );
+}
+
+export default App;
