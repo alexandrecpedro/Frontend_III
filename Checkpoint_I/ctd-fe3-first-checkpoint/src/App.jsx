b0332f1df058f52c7e75abf4385424531f69a412
Alexandre Pedro <alexandrecpedro@gmail.com>
2022-11-09 13:28:03 -0300

    Checkpoint I

--- Checkpoint_I/ctd-fe3-first-checkpoint/src/App.jsx
+++ Checkpoint_I/ctd-fe3-first-checkpoint/src/App.jsx
@@ -1,93 +1,96 @@
-import { useState } from 'react'
-import { Card } from '../src/Card'
-import { players } from './database/players'
+import { useState } from "react";
+import { Card } from "./components/Card";
 
-function App() {
-  const [playerName, setPlayerName] = useState('')
-  const [position, setPosition] = useState('')
-  const [playerPhoto, setPlayerPhoto] = useState('')
-  const [formsError, setFormsError] = useState(false)
-  const [allPlayers, setAllPlayers] = useState(players)
-
-  function registerPlayer(event) {
-    event.preventDefault()
-
-    const newRegisteredPlayer = {
-      name: playerName,
-      position: position,
-      picture: playerPhoto
-    }
-
-    if (playerName === '' || position === '') {
-      setFormsError(true)
-    } else {
-      setFormsError(false)
-
-      setAllPlayers([...allPlayers, newRegisteredPlayer])
-
-      setPlayerName('')
-      position('')
-      playerPhoto('')
-    }
-  }
-
-  return (
-    <main className="component">
-      <div className="tittle-wrapper">
-        <h1>Seleção Portuguesa Copa 2022</h1>
-        <h2>Aqui é você quem manda! Convoque os jogadores</h2>
-        <p>
-          Selecione a foto no link{' '}
-          <a href="https://lnkd.in/d-v-X-e7">https://lnkd.in/d-v-X-e7</a>
-        </p>
-      </div>
-
-      <form
-        className={formsError ? 'form-error' : ''}
-        onSubmit={event => registerPlayer(event)}
-      >
-        <div>
-          <label htmlFor="playerName">Nome: </label>
-          <input
-            id="playerName"
-            type="text"
-            value={playerName}
-            onChange={event => setPlayerName(event.target.value)}
-          />
-        </div>
-
-        <div>
-          <label htmlFor="position">Posicão: </label>
-          <input
-            id="position"
-            type="text"
-            value={position}
-            onChange={event => setPosition(event.target.value)}
-          />
-        </div>
-
-        <div>
-          <label htmlFor="playerPhoto">Link da Foto: </label>
-          <input
-            id="playerPhoto"
-            type="text"
-            value={playerPhoto}
-            onChange={event => setPlayerPhoto(event.target.value)}
-          />
-        </div>
-
-        <button type="submit">Convocar Jogador</button>
-      </form>
-
-      {formsError ? <span>O seu formulário contem erros</span> : null}
-
-      <section className="players">
-        {allPlayers.map(player => {
-          return <Card playerData={player} />
-        })}
-      </section>
-    </main>
-  )
+// Aqui você irá escrever as suas funções de Validação, para verificar se o Formulário foi preenchido corretamente
+function validacaoNomeDaCor(nomeCor) {
+  return nomeCor.trim().length > 2;
 }
 
-export default App
+function validacaoCorHexadecimal(corHexadecimal) {
+  // const padraoCorHexadecimal = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+  const padraoCorHexadecimal = /^#(?:[0-9a-f]{3}){1,2}$/i;
+  return padraoCorHexadecimal.test(corHexadecimal);
+}
+
+function App() {
+  // Aqui você irá criar os Estados para manipular os Inputs
+  const [nomeCor, setNomeCor] = useState("");
+  const [corHexadecimal, setCorHexadecimal] = useState("");
+  const [cores, setCores] = useState([]);
+  const [formularioErro, setFormularioErro] = useState(false);
+
+  const cadastrarCor = (event) => {
+    event.preventDefault();
+    if (
+      validacaoCorHexadecimal(corHexadecimal) &&
+      validacaoNomeDaCor(nomeCor)
+    ) {
+      setFormularioErro(false);
+      let newId = cores.length;
+
+      const novaCorCadastrada = {
+        id: newId,
+        nomeCor: nomeCor,
+        corHexadecimal: corHexadecimal,
+      };
+
+      setCores([...cores, novaCorCadastrada]);
+      setNomeCor("");
+      setCorHexadecimal("");
+    } else {
+      setFormularioErro(true);
+    }
+  };
+
+  return (
+    <div className="App">
+      <section className={formularioErro ? "form-error" : ""}>
+        <div className="tittle-wrapper">
+          <h1>ADICIONAR NOVA COR</h1>
+        </div>
+
+        <form onSubmit={(event) => cadastrarCor(event)}>
+          {/* Comece a desenvolver o seu Código por aqui :) */}
+          <div className="input">
+            <label htmlFor="nomeCor">Nome</label>
+            <input
+              id="nomeCor"
+              type="text"
+              value={nomeCor}
+              onChange={(event) => setNomeCor(event.target.value)}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="corHexadecimal">Cor</label>
+            <input
+              id="corHexadecimal"
+              placeholder="Insira sua cor no formato hexadecimal"
+              type="color"
+              value={corHexadecimal}
+              onChange={(event) => setCorHexadecimal(event.target.value)}
+            />
+          </div>
+
+          <button type="submit">Adicionar cor</button>
+        </form>
+      </section>
+
+      {formularioErro ? (
+        <span>Por favor, verifique os dados inseridos no formulário</span>
+      ) : null}
+
+      <div className="tittle-wrapper">
+        <h1>CORES FAVORITAS</h1>
+      </div>
+
+      <section>
+        {cores.map((cor, index) => (
+          <Card key={index} corData={cor} />
+        ))}
+      </section>
+    </div>
+  );
+}
+
+export default App;
